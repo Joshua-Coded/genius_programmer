c2fee52354461555a059a2996e831cbe732416a5
Joshua-Coded <j.alana@alustudent.com>
2026-02-16 18:41:36 +0200

    a simple program that create a calendar

---
+++ C_Basics/calendar.c
@@ -0,0 +1,17 @@
+/* A simple program that print a calendar   */
+// by Joshua Alana
+// Date: 16/02/2026
+
+#include <stdio.h>
+
+int main()
+{
+printf("\nSun\tMon\tTue\tWed\tThu\tFri\tSat\n");
+printf("\t\t\t\t1\t2\t3\n");
+printf("4\t5\t6\t7\t8\t9\t10\n");
+printf("11\t12\t13\t14\t15\t15\t16\t17\n");
+printf("18\t19\t20\t21\t22\t23\t24\n");
+printf("25\t26\t27\t28\t29\t30\t31\n");
+
+
+}
--- C_Basics/chapter1.c
+++ C_Basics/chapter1.c
@@ -6,5 +6,5 @@
 
 int main() {
 
-printf("Hello World!\n");
+printf("Hello World!\n\t");
 }
