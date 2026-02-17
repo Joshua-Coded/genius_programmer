c4746fac60606cb9973ea76f5bc186c9cb946ed4
Joshua-Coded <j.alana@alustudent.com>
2026-02-17 19:25:47 +0200

    chapter 2

---
+++ C_Basics/chapter2.c
@@ -0,0 +1,25 @@
+/* Chapter 2 Concepts */
+// By Joshua Alana
+// Date 17/02/2026
+
+#include <stdio.h>
+
+int main() {
+int x;
+float y;
+char c;
+
+// variable initialization
+
+x = -4436;
+y = 55.65;
+c = 'M';
+
+// printing the variable contents to standard output
+
+printf("\n The value of integer variable x is %d", x);
+printf("\n The value of float variable y i %.2f", y);
+printf("\n The value of character variable c is %c\n", c); 
+
+
+}
