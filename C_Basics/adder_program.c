c212a784ff628eb9c52b4754374489b460f91e7f
Joshua-Coded <j.alana@alustudent.com>
2026-02-17 21:57:50 +0200

    adder program

---
+++ C_Basics/adder_program.c
@@ -0,0 +1,21 @@
+/* Learning C again */
+// By Joshua Alana
+// Dates 17/02/2026
+
+#include <stdio.h>
+
+int main(){
+
+int intOperand1 = 0;
+int intOperand2 = 0;
+
+
+printf("\n\tAdder Program, By Joshua Alana\n");
+printf("Enter first operand\n");
+scanf("%d", &intOperand1);
+printf("Enter Second Operand\n");
+scanf("%d", &intOperand2);
+
+printf("The result is %d", intOperand1 + intOperand2);
+
+}
