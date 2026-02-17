4f06eb3e6d23c1d59de82a03acddd17775d7c026
Joshua-Coded <j.alana@alustudent.com>
2026-02-17 19:40:24 +0200

    constansts

--- C_Basics/chapter2.c
+++ C_Basics/chapter2.c
@@ -5,21 +5,24 @@
 #include <stdio.h>
 
 int main() {
-int x;
-float y;
-char c;
+//float y;
+//char c;
 
 // variable initialization
 
-x = -4436;
-y = 55.65;
-c = 'M';
+//x = -4436;
+//y = 55.65;
+//c = 'M';
 
 // printing the variable contents to standard output
 
-printf("\n The value of integer variable x is %d", x);
-printf("\n The value of float variable y i %.2f", y);
-printf("\n The value of character variable c is %c\n", c); 
+//printf("\n The value of integer variable x is %d", x);
+//printf("\n The value of float variable y i %.2f", y);
+//printf("\n The value of character variable c is %c\n", c); 
 
+const int x = 20;
+const float PI = 3.142;
+
+printf("\n Constants values are %d and %.2f\n", x, PI);
 
 }
