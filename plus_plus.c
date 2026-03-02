1a4bcef80bc7d5146becce11b412a76795dc21c7
Joshua-Coded <j.alana@alustudent.com>
2026-03-02 14:40:35 +0200

    some code on incrementro

---
+++ plus_plus.c
@@ -0,0 +1,12 @@
+/* A incrementor programme */
+// By Joshua Alana
+// Date: 2/03/2026
+
+#include <stdio.h>
+
+int main() {
+    int x = 0;
+    printf("\n The value of X is %d", x);
+    x++;
+    printf("\nThe value of X is %d", x);
+}
