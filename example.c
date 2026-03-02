57a0b72f1988e9fce7b33211e6530d298e6489fc
Joshua-Coded <j.alana@alustudent.com>
2026-03-02 15:22:23 +0200

    example 4

--- example.c
+++ example.c
@@ -4,12 +4,22 @@ int main() {
     int x = 1;
     int y = 2;
 
+    // x = y * x + 1;
+    // printf("\nThe value of X is : %d\n", x);
+
+    // x = 1;
+    // y = 2;
+
+    // x += y * x + 1;
+    // printf("\n The value of x right now is: %d\n", x);
+
     x = y * x + 1;
-    printf("\nThe value of X is : %d\n", x);
+    printf("\n The value of x is: %d\n", x);
 
     x = 1;
     y = 2;
 
-    x += y * x + 1;
-    printf("\n The value of x right now is: %d\n", x);
+    x -= y * x + 1;
+    printf("\nThe value of x right now is: %d\n", x);
+
 }
