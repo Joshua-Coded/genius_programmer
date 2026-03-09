2754bf85c5651a226bbce8f34870cb24d7838afc
Joshua-Coded <j.alana@alustudent.com>
2026-03-09 14:40:05 +0200

    some simple code

---
+++ arrays/file.c
@@ -0,0 +1,23 @@
+/* one way of initialising array*/
+
+// int iArray[10] = {1,2,3,4,5};
+
+/* another way of initialising the  array using for loop*/
+
+#include <stdio.h>
+
+int main () {
+    int x;
+    int iArray[5] = {1,2,3,4,5};
+
+// initialise array elements
+    // for (x = 0; x < 5; x++)
+    //     iArray[x] = x;
+    
+
+// print array elements contents
+    for (x = 0; x <= iArray[x]; x++)
+        printf("\nThe value of iArray index %d is %d\n", x, x);
+
+
+}
