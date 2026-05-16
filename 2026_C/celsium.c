f14d63f64787c329e30eefcb80b2b7ff0a10fe98
Joshua-Coded <j.alana@alustudent.com>
2026-05-16 03:38:11 +0200

    learning C agains

--- 2026_C/celsium.c
+++ 2026_C/celsium.c
@@ -12,13 +12,13 @@ int main() {
     upper = 300;
     step = 20;
 
-    fahr = lower;
+    fahr = upper;
 
     printf("This is Fahrenheit Conversion to Celsius\n");
 
-    while (fahr <= upper) {
+    while (fahr >= lower) {
         celsius = 5 * (fahr - 32) / 9;
         printf("%3d\t%6d\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr - step;
     }
 }
