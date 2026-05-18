4b91e565bbd6ca4a48ad31b1b2a1a98d523b0446
Joshua-Coded <j.alana@alustudent.com>
2026-05-18 22:31:10 +0200

    some codes on C

--- 2026_C/celsium.c
+++ 2026_C/celsium.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 /* Print Fahrenheit- Celsius Table 
     for fahr = 0, 20, ....., 300
 */
 
 int main() {
     int fahr, celsius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = upper;
+    fahr = UPPER;
 
     printf("This is Fahrenheit Conversion to Celsius\n");
 
-    while (fahr >= lower) {
+    while (fahr >= LOWER) {
         celsius = 5 * (fahr - 32) / 9;
         printf("%3d\t%6d\n", fahr, celsius);
-        fahr = fahr - step;
+        fahr = fahr - STEP;
     }
 }
